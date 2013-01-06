aa049f0cb13a701b3ccdba0ada49e9751a7d2db2
Andy Waite <github.aw@andywaite.com>
2013-01-06 20:20:27 +0000

    Clean-up comments

--- app/auto.rb
+++ app/auto.rb
@@ -1,37 +1,26 @@
 class Auto
   def initialize
-    # Create an account store object.
     @account_store = ACAccountStore.alloc.init
-
-    # Create an account type that ensures Twitter accounts are retrieved.
     @account_type = @account_store.accountTypeWithAccountTypeIdentifier ACAccountTypeIdentifierTwitter
 
-    # Request access from the user to use their Twitter accounts.
     options = nil # required for some account types but not Twitter
     error_ptr = Pointer.new(:object)
     @account_store.requestAccessToAccountsWithType @account_type, options:options, completion: lambda { |granted, error_ptr|
       if granted
-        # Get the list of Twitter accounts.
         accounts = @account_store.accountsWithAccountType @account_type
+        raise "No accounts" if accounts.empty? # I don't think should ever happen
 
         # For the sake of brevity, we'll assume there is only one Twitter account present.
         # You would ideally ask the user which account they want to tweet from, if there is
         # more than one Twitter account present.
 
-        raise "No accounts" if accounts.empty? # I don't think should ever happen
-
         # Grab the initial Twitter account to tweet from.
         account = accounts.first
-
-        # Create a request, which in this example, posts a tweet to the user's timeline.
-        # This example uses version 1 of the Twitter API.
-        # This may need to be changed to whichever version is currently appropriate.
 
         url = NSURL.URLWithString "http://api.twitter.com/1.1/statuses/update.json"
         params = { status: "Test post from iOS" }
         post_request = TWRequest.alloc.initWithURL url, parameters:params, requestMethod:TWRequestMethodPOST
 
-        # Set the account used to post the tweet.
         post_request.account = account
 
         # Perform the request created above and create a handler block to handle the response.
