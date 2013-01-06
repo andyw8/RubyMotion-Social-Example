df924dc39d1dd0079a141d35b98ce816c60c9559
Andy Waite <github.aw@andywaite.com>
2013-01-06 23:58:53 +0000

    Extract error handling

--- app/auto.rb
+++ app/auto.rb
@@ -29,28 +29,32 @@ class Auto
         }
       else
         NSLog "Not Granted"
-        case error_ptr.code
-        when ACErrorUnknown
-          NSLog "Error Unknown"
-        when ACErrorAccountMissingRequiredProperty
-          NSLog "Account missing required property"
-        when ACErrorAccountAuthenticationFailed
-          NSLog "Account authentication failed"
-        when ACErrorAccountTypeInvalid
-          NSLog "Account type invalid"
-        when ACErrorAccountAlreadyExists
-          NSLog "Account already exists"
-        when ACErrorAccountNotFound
-          # Occurs if the user has no accounts set up of the particular type
-          NSLog "Account not found"
-        when ACErrorPermissionDenied
-          NSLog "User denied permission"
-        when ACErrorAccessInfoInvalid
-          NSLog "Access info invalid"
-        else
-          NSLog "Unknown error code"
-        end
+        handle_error error_ptr
       end
     }
+  end
+
+  def handle_error(error_ptr)
+    case error_ptr.code
+    when ACErrorUnknown
+      NSLog "Error Unknown"
+    when ACErrorAccountMissingRequiredProperty
+      NSLog "Account missing required property"
+    when ACErrorAccountAuthenticationFailed
+      NSLog "Account authentication failed"
+    when ACErrorAccountTypeInvalid
+      NSLog "Account type invalid"
+    when ACErrorAccountAlreadyExists
+      NSLog "Account already exists"
+    when ACErrorAccountNotFound
+      # Occurs if the user has no accounts set up of the particular type
+      NSLog "Account not found"
+    when ACErrorPermissionDenied
+      NSLog "User denied permission"
+    when ACErrorAccessInfoInvalid
+      NSLog "Access info invalid"
+    else
+      NSLog "Unknown error code"
+    end
   end
 end
