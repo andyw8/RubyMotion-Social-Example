a1108e4161f4d33ab10fde68b12c4ea6f4af6fb7
Andy Waite <github.aw@andywaite.com>
2013-01-06 20:08:03 +0000

    Better error handling

--- app/auto.rb
+++ app/auto.rb
@@ -8,7 +8,8 @@ class Auto
 
     # Request access from the user to use their Twitter accounts.
     options = nil # required for some account types but not Twitter
-    @account_store.requestAccessToAccountsWithType @account_type, options:options, completion: lambda { |granted, error|
+    error_ptr = Pointer.new(:object)
+    @account_store.requestAccessToAccountsWithType @account_type, options:options, completion: lambda { |granted, error_ptr|
       if granted
         # Get the list of Twitter accounts.
         accounts = @account_store.accountsWithAccountType @account_type
@@ -16,30 +17,50 @@ class Auto
         # For the sake of brevity, we'll assume there is only one Twitter account present.
         # You would ideally ask the user which account they want to tweet from, if there is
         # more than one Twitter account present.
-        if accounts.any?
-          # Grab the initial Twitter account to tweet from.
-          account = accounts.first
 
-          # Create a request, which in this example, posts a tweet to the user's timeline.
-          # This example uses version 1 of the Twitter API.
-          # This may need to be changed to whichever version is currently appropriate.
+        raise "No accounts" if accounts.empty? # I don't think should ever happen
 
-          url = NSURL.URLWithString "http://api.twitter.com/1.1/statuses/update.json"
-          params = { status: "Test post from iOS" }
-          post_request = TWRequest.alloc.initWithURL url, parameters:params, requestMethod:TWRequestMethodPOST
+        # Grab the initial Twitter account to tweet from.
+        account = accounts.first
 
-          # Set the account used to post the tweet.
-          post_request.account = account
+        # Create a request, which in this example, posts a tweet to the user's timeline.
+        # This example uses version 1 of the Twitter API.
+        # This may need to be changed to whichever version is currently appropriate.
 
-          # Perform the request created above and create a handler block to handle the response.
-          post_request.performRequestWithHandler lambda { |response_data, url_response, error|
-            NSLog "HTTP response status: #{url_response.statusCode}"
-          }
-        else
-          NSLog "No Accounts"
-        end
+        url = NSURL.URLWithString "http://api.twitter.com/1.1/statuses/update.json"
+        params = { status: "Test post from iOS" }
+        post_request = TWRequest.alloc.initWithURL url, parameters:params, requestMethod:TWRequestMethodPOST
+
+        # Set the account used to post the tweet.
+        post_request.account = account
+
+        # Perform the request created above and create a handler block to handle the response.
+        post_request.performRequestWithHandler lambda { |response_data, url_response, error|
+          NSLog "HTTP response status: #{url_response.statusCode}"
+        }
       else
         NSLog "Not Granted"
+        case error_ptr.code
+        when ACErrorUnknown
+          NSLog "Unknown error"
+        when ACErrorAccountMissingRequiredProperty
+          NSLog "Account Missing Required Property"
+        when ACErrorAccountAuthenticationFailed
+          NSLog "Account Authentication Failed"
+        when ACErrorAccountTypeInvalid
+          NSLog "Account Type Invalid"
+        when ACErrorAccountAlreadyExists
+          NSLog "Account Already Exists"
+        when ACErrorAccountNotFound
+          # Occurs if the user has no accounts set up of the particular type
+          NSLog "Account Not Found"
+        when ACErrorPermissionDenied
+          NSLog "User denied permission"
+        when ACErrorAccessInfoInvalid
+          NSLog "Access Info Invalid"
+        else
+          NSLog "Unknown error code"
+        end
       end
     }
   end
